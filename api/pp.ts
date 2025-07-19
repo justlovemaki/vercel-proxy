9be545d339aaf6bb25e01c5822a894a983fe16ee
何夕2077 <274166795@qq.com>
2025-07-19 16:14:19 +0800

    Update pp.ts

--- api/pp.ts
+++ api/pp.ts
@@ -50,7 +50,7 @@ export default async function handler(request: Request) {
   headers.delete('host');
   headers.set('X-Forwarded-Host', url.host); // 仍然使用原始请求的 host
   headers.set('X-Forwarded-Proto', url.protocol.slice(0, -1));
-  headers.set('User-Agent', 'v2rayN');
+  headers.set('User-Agent', 'clash-verge/v1.6.6');
 
 
   try {
